fa464d8588ee5ee9056a24354054a98632410f9d
nyikonzoli <33204609+nyikonzoli@users.noreply.github.com>
2022-06-14 23:04:28 +0200

    backend login base

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/admin/login', function () {
+    return view('admin.login');
 });
+
+Route::post('/admin/login', [AuthController::class, 'login'])->name('auth.login');
