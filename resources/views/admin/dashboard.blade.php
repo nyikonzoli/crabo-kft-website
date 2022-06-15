76220f3d451b0e37e880bf71cbda870e06fcc0a4
nyikonzoli <33204609+nyikonzoli@users.noreply.github.com>
2022-06-15 14:18:49 +0200

    admin index base

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,12 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Admin dashboard    
-</body>
-</html>
+@extends('layouts.admin')
+
+@section('title', 'Kezelőfelület')
+
+@section('head')
+    <link rel="stylesheet" href="{{ asset('css/admin/index.css') }}">  
+@endsection
+
+@section('content')
+    <div class="content col-lg-8">
+        <div class="top-text box-shape">
+            <h1>Üdvözöllek az adminisztrációs felületen!</h1>
+        </div>
+        <div class="boxes">
+            <div class="box box-shape box-black">
+                <h1>19</h1>
+                <p>jármű összesen</p>
+            </div>
+            <div class="box box-shape box-blue clickable">
+                <h1>12</h1>
+                <p>személyautó</p>
+            </div>
+            <div class="box box-shape box-pink clickable">
+                <h1>7</h1>
+                <p>teherautó</p>
+            </div>
+            <div class="box box-shape box-red clickable">
+                <h1>0</h1>
+                <p>elektromos autó</p>
+            </div>
+        </div>
+    </div>
+@endsection
